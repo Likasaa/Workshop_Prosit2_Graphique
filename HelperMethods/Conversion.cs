b6a30317e0022a428acc60d109e0c8d21bb09725
Maxime EY <maxime.ey@viacesi.fr>
2022-11-17 18:24:04 +0100

    Add project files.

---
+++ HelperMethods/Conversion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workshop_Prosit2_Graphique.HelperMethods
+{
+    class Conversion
+    {
+        public string Convert(string ChainToConvert, string Converted)
+        {
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(ChainToConvert);
+            for (int i = 0; i < ChainToConvert.Length; i++)
+            {
+                if (asciiBytes[i] < 123 && asciiBytes[i] > 96)
+                {
+                    int convertion = asciiBytes[i] - 32;
+                    char character = (char)convertion;
+                    Converted += character.ToString();
+                }
+                else
+                {
+                    char character = (char)asciiBytes[i];
+                    Converted += character.ToString();
+                }
+            }
+            Console.WriteLine(Converted);
+            return Converted;
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Workshop_Prosit2_Graphique.HelperMethods;
+
+namespace Workshop_Prosit2_Graphique
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static Conversion Conversion = new Conversion();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NameInputButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = MainTextBox.Text;
+            string witness = "";
+            string result = Conversion.Convert(name, witness);
+            NameLabel.Content = name + " string transformed into " + result + " string";
+        }
+    }
+}
